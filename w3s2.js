e281be22ae9e305980aa54bf783bd5ded42903a3
SachinMudannayaka <sachinvimu2018@gmail.com>
2023-01-19 01:45:13 +0530

    Reactor first update

---
+++ w3s2.js
@@ -0,0 +1,4 @@
+function myFunction(){document.getElementById('ch').innerHTML="VIMUKTHI";}
+function myFunction2(){{document.getElementById('myImage').src="cry.jpg"}
+	                   {document.getElementById('myImage').src="celebration.png"}}
+		
